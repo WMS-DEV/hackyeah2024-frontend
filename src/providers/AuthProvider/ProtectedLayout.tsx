fef290130cdcf2329580889166be0cdc24a3c44d
Vladyslav <vlad09vlad09@gmail.com>
2024-09-28 17:03:15 +0200

    fix: solve confints

--- src/providers/AuthProvider/ProtectedLayout.tsx
+++ src/providers/AuthProvider/ProtectedLayout.tsx
@@ -1,24 +1,28 @@
-import { useNavigate, useOutlet } from "react-router-dom";
-import { useAuth } from "./AuthProvider";
-import { useEffect } from "react";
-import Navbar from "../../components/Navbar/Navbar";
-import "./ProtectedLayout.scss";
+import { useNavigate, useOutlet } from 'react-router-dom';
+import { useAuth } from './AuthProvider';
+import { useEffect } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import './ProtectedLayout.scss';
+import Slider from '../../components/Slider/Slider';
 
 export const ProtectedLayout = () => {
-  const { isTokenValid } = useAuth();
-  const outlet = useOutlet();
-  const navigate = useNavigate();
+    const { isTokenValid } = useAuth();
+    const outlet = useOutlet();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!isTokenValid()) {
-      navigate("/");
-    }
-  }, [isTokenValid, navigate]);
+    useEffect(() => {
+        if (!isTokenValid()) {
+            navigate('/');
+        }
+    }, [isTokenValid, navigate]);
 
-  return (
-    <div className="protected-layout">
-      <Navbar />
-      {outlet}
-    </div>
-  );
+    return (
+        <div className="protected-layout">
+            <Navbar />
+            <Slider>
+                <h2>please works</h2>
+            </Slider>
+            {outlet}
+        </div>
+    );
 };
