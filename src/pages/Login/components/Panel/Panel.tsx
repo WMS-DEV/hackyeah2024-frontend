08a60cfc330c85d8b27d3e08336ae16eed82828d
Vladyslav <vlad09vlad09@gmail.com>
2024-09-29 06:49:48 +0200

    feat: add logo

--- src/pages/Login/components/Panel/Panel.tsx
+++ src/pages/Login/components/Panel/Panel.tsx
@@ -1,41 +1,34 @@
 // import { navigateToGoogleAuth } from "../../../../api/googleAuth";
-import "./Panel.scss";
-import googleLogo from "@/assets/google-logo.webp";
-import spotlyLogo from "@/assets/sportlylogo.webp";
+import './Panel.scss';
+import googleLogo from '@/assets/google-logo.webp';
+import spotlyLogo from '@/assets/logoSpotly.png';
 
 const LoginPanel = () => {
-  const handleGoogleLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    // navigateToGoogleAuth();
-    localStorage.setItem("isAuthenticated", "true");
-    window.location.reload();
-  };
+    const handleGoogleLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        // navigateToGoogleAuth();
+        localStorage.setItem('isAuthenticated', 'true');
+        window.location.reload();
+    };
 
-  return (
-    <div className="login-panel">
-      <div className="login-panel__header">Sign in to preview</div>
-      <div className="login-panel__body">
-        <button
-          className="login-panel__body__button"
-          onClick={handleGoogleLogin}
-        >
-          <img
-            src={googleLogo}
-            alt="Google logo"
-            className="login-panel__body__button__logo"
-            width="40"
-          />
-          <span className="login-panel__body__button__text">
-            Sign in with Google{" "}
-          </span>
-        </button>
-      </div>
-      <div>
-        <img className="login-panel__sportly-logo" alt="Sportly logo"
-          src={spotlyLogo}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="login-panel">
+            <div className="login-panel__header">Sign in to preview</div>
+            <div className="login-panel__body">
+                <button className="login-panel__body__button" onClick={handleGoogleLogin}>
+                    <img
+                        src={googleLogo}
+                        alt="Google logo"
+                        className="login-panel__body__button__logo"
+                        width="40"
+                    />
+                    <span className="login-panel__body__button__text">Sign in with Google</span>
+                </button>
+            </div>
+            <div>
+                <img className="login-panel__sportly-logo" alt="Sportly logo" src={spotlyLogo} />
+            </div>
+        </div>
+    );
 };
 export default LoginPanel;
