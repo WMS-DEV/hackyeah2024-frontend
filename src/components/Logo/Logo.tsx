bafccb8f439b2f4adedbdb3df34ed16e43e63f6b
Vladyslav <vlad09vlad09@gmail.com>
2024-09-29 10:52:48 +0200

    feat: update logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/sportlyLogo2.png';
 import './Logo.style.scss';
+import { useEvents } from '../../providers/EventsProvider/EventsProvider';
 const Logo = () => {
-    return <img src={logo} className="logo" alt="logo" />;
+    const navigate = useNavigate();
+    const { removeDrafts } = useEvents();
+
+    const handleClick = () => {
+        removeDrafts();
+        navigate('/home');
+    };
+
+    return <img src={logo} onClick={handleClick} className="logo" alt="logo" />;
 };
 
 export default Logo;
